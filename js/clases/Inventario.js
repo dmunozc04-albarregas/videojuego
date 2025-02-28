ebb23091361c75fdd81e38b85d8ced79b59d37b4
dmunozc04-albarregas <dmunozc04@iesalbarregas.es>
2025-02-28 16:26:55 +0100

    Add y remove de inventario

--- js/clases/Inventario.js
+++ js/clases/Inventario.js
@@ -1,3 +1,4 @@
+import { Arma } from "./Arma.js"
 class Inventario{
     #armasCompradas = [];
 
@@ -13,6 +14,27 @@ class Inventario{
         this.armasCompradas = armasCompradas;
     }
 
-    
+    addEnemigos(enemigo) {
+        if (!(enemigo instanceof Arma)) {
+            console.error("Solo se pueden agregar objetos de la clase Arma.");
+        }
+        else if (this.enemigos.length >= this.tamanioEnemigos) {
+            console.error("El array está lleno.");
+        }
+        else if (this.enemigos.includes(armasCompradas)) {
+            console.error(`El arma ${armasCompradas.nombre} ya está en el array.`);
+        }
+        else {
+            this.enemigos.push(enemigo);
+        }
+    }
 
+    removeEnemigos(nombreArma) {
+        const indice = this.enemigos.findIndex(armasCompradas => armasCompradas.nombre === nombreArma);
+        if (indice !== -1) {
+            this.enemigos.splice(indice, 1); 
+        } else {
+            console.error(`El enemigo ${nombreArma} no está en el array.`);
+        }
+    }
 }
