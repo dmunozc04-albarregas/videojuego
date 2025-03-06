4b55facf6f32540b8554ec423f6442b746b8f3cf
Adrian Perez <adrianperez936@gmail.com>
2025-03-06 20:46:30 +0100

    musica arreglada y fondo creador

--- js/controladores/ControladorCrearPersonaje.js
+++ js/controladores/ControladorCrearPersonaje.js
@@ -4,3 +4,56 @@ import { Jugador } from "../clases/Jugador.js";
 // Código para controlar la música
 const musica = new Musica();
 musica.reproducir("../recursos/sonidos/Creador.mp3");
+
+const avatares = document.querySelectorAll(".avatar");
+const sonidoHover = new Audio("../recursos/sonidos/hover-sound.mp3");
+const btnEnviar = document.querySelector(".btn-enviar");
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", crearPersonaje);
+
+    cargarAvatares();
+  });
+
+// Agregar evento para reproducir sonido al pasar el mouse
+btnEnviar.addEventListener("mouseenter", () => {
+    sonidoHover.currentTime = 0; // Reiniciar el audio para que se escuche cada vez
+    sonidoHover.play();
+  });
+
+
+  const img_avatares = [
+    "../recursos/imagenes/Ciri.webp",
+    "../recursos/imagenes/personajes/radovid.webp",
+    "../recursos/imagenes/personajes/triss.webp",
+    "../recursos/imagenes/personajes/shani.webp",
+  ];
+
+  function cargarAvatares() {
+    for (let i = 0; i < img_avatares.length; i++) {
+      avatares[i].src = img_avatares[i];
+    }};
+
+
+function crearPersonaje() {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    iconColor: "white",
+    customClass: {
+      popup: "colored-toast",
+    },
+    showConfirmButton: false,
+    timer: 2000, 
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+
+  Toast.fire({
+    icon: "success",
+    title: "Personaje creado correctamente",
+  });
+}
