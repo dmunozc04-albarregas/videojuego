98660173e064e61044662bc7fdcecb6bad98b688
dmunozc04-albarregas <dmunozc04@iesalbarregas.es>
2025-03-23 22:56:23 +0100

    Creación de una parte del recuadro lateral para el personaje en el lobby

--- js/controladores/ControladorLobby.js
+++ js/controladores/ControladorLobby.js
@@ -13,4 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("personaje").src = personaje.imagen;
   // Código para cargar el nombre del personaje
   document.getElementById("nombre-personaje").innerText = personaje.nombre;
+
+  document.getElementById("vida-personaje").innerText = personaje.vida;
+
+  document.getElementById("fuerza-personaje").innerText = personaje.fuerza;
+
+  document.getElementById("magia-personaje").innerText = personaje.magia;
+
+  document.getElementById("nivel-personaje").innerText = personaje.nivel;
+  const barra_vida_personaje = document.getElementById("barra-vida-personaje")
+  barra_vida_personaje.value = personaje.vida;
+
+  if(personaje.vida === 100){
+    barra_vida_personaje.classList.add("barra-verde")
+  }
+
+
 });
